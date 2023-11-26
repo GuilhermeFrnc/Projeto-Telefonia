2d96fa0088b06726235f21aa7e9678871a8641c7
Nicolas <nicolastrindade0902@gmail.com>
2023-11-25 23:00:08 -0300

    Commit feito 3. Método Recarga - Classe PréPago.

--- src/main/java/telefonia/PrePago.java
+++ src/main/java/telefonia/PrePago.java
@@ -1,21 +1,33 @@
 package telefonia;
 
-import java.util.ArrayList;
-
+import java.util.GregorianCalendar;
 
 public class PrePago extends Assinante {
 
-    private float creditos;
+    private float credito;
     private int numRecargas;
+    private float[] recargas;
 
 
     public PrePago(long cpf, String nome, int numero) {
         super(cpf, nome, numero);
     }
 
-
-	public void recarregar(Recarga recarga) {
-		// TODO Auto-generated method stub
+//Método para a recarga
+	public void recarregar(GregorianCalendar data, float valor) {
+		
+		if (numRecargas < recargas.length) {     // IF para verificar se o número de recargas é menor que o tamanho do vetor Recargas.
+			
+			recargas[numRecargas] = valor;
+			
+			numRecargas++;
+			credito += valor;
+			
+			System.out.println("Recarga acaba de ser registrada!");
+		}
+		else {
+			System.out.println("Não foi possível efetuar a recarga. O limite foi atingido.");
 		
 	}
+  }
 }

9d0e8a03a367e73c9a249c6fa4db2b72a714d93c
Nicolas <110908637+YngFlexx@users.noreply.github.com>
2023-11-26 00:17:19 -0300

    Substituindo ArrayList por Vetor. Commit 6.

--- src/main/java/telefonia/Assinante.java
+++ src/main/java/telefonia/Assinante.java
@@ -1,25 +1,32 @@
 package telefonia;
 
-import java.util.ArrayList;
-
 public class Assinante {
 
     private long cpf;
     private String nome;
     private int numero;
-    protected ArrayList<Chamada> chamadas;    // Lista para adicionar o registro das chamadas feitas.
-    protected int numChamadas;    // contador.
+    protected Chamada[] chamadas;  // Vetor para armazenar as chamadas.
+    protected int numChamadas;     // Contador
 
-    public Assinante(long cpf, String nome, int numero) {
+    public Assinante(long cpf, String nome, int numero, int maxChamadas) {
         this.cpf = cpf;
         this.nome = nome;
         this.numero = numero;
-        this.chamadas = new ArrayList<>();   // Cria uma lista de chamdas para cada assinante que for criado.
+        this.chamadas = new Chamada[maxChamadas];  // Inicializando o vetor com o tamanho máximo
         this.numChamadas = 0;
     }
 
-    public long getCpf() {
+    public long getCpf() {      // Retornando o número do CPF
         return this.cpf;
+    }
+
+    public void adicionarChamada(Chamada chamada) {
+        if (numChamadas < chamadas.length) {
+            chamadas[numChamadas] = chamada; //Verificando se há espaço no vetor para armazenar novas chamadas
+            numChamadas++;
+        } else {
+            System.out.println("Sem espaço para adicionar mais chamadas.");
+        }
     }
 
     @Override
